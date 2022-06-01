d73ef8fbf40157cc24f32ac05542ab422c1199e7
Qasim-Elhabrush <qasimelhabrush@gmail.com>
2022-06-01 03:59:25 -0400

    ui increased. Assignment finished.

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("working");
+let signUpButton = document.getElementById("createAccount ");
+
+
+
+function User(firstName, lastName, email, phoneNumber, password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phoneNumber = phoneNumber;
+    this.password = password;
+}
+let submit = function() {
+    let password = document.getElementById("password ").value;
+    let confirmPassword = document.getElementById("confirmPassword ").value;
+    if (password != confirmPassword) {
+        document.getElementById("password ").className = "error";
+        document.getElementById("confirmPassword ").className = "error";
+        alert("Passwords must match!")
+    } else if (password == confirmPassword) {
+        let fName = document.getElementById("fName").value
+        fName = new User(document.getElementById("fName").value, document.getElementById("lName ").value, document.getElementById("email ").value, document.getElementById("phoneNumber ").value, document.getElementById("password ").value);
+        login(fName);
+    }
+
+}
+signUpButton.addEventListener("mousedown", () => signUpButton.style.backgroundColor = "rgb(52, 163, 52)");
+signUpButton.addEventListener("mouseup", () => signUpButton.style.backgroundColor = "#596D48");
+signUpButton.addEventListener("click", submit);
+
+login = function(firstName) {
+    alert(`Hello ${firstName.firstName}! Thank you for signing up on this website!`);
+}
